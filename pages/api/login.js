a6970ebc45583e9b03efdb62118e122726ff7ed7
rainforss <shengyan@ualberta.ca>
2021-05-06 19:08:02 -0600

    Finished registration and authentication

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,6 @@
 import User from "../../models/user";
 import withSession from "../../middleware/session";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export default withSession(async (req, res) => {
   try {
